a4ad684b1d77c0610bb92a72ad6afae4a86f5b3a
Aleksey <arekabu@yandex.com>
2025-06-02 12:44:00 +0300

    CKEditor installed, migrationse made

---
+++ post/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=19, unique=True)
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=100)
+    text = RichTextUploadingField()
+
+
+class PostCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField()
+    approved = models.BooleanField(default=False)
+
