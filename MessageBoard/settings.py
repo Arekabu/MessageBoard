a1230d6967b4a170880db223c842299568ab66c6
Arex <arekabu@yandex.com>
2025-06-02 22:47:53 +0300

    html5video implemented

--- MessageBoard/settings.py
+++ MessageBoard/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -133,7 +134,7 @@ CKEDITOR_CONFIGS = {
         'toolbar': 'full',
         'filebrowserBrowseUrl': '/ckeditor/browse/',  # Важно!
         'filebrowserUploadUrl': '/ckeditor/upload/',  # Важно!
-        'extraPlugins': 'Html5video',  # Активирует плагин
+        'extraPlugins': 'html5video',  # Активирует плагин
 
     }
 }
